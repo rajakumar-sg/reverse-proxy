c87c6697cb33f84ceaa7bce527adbe2cec23cf40
raj <torajakumar@gmail.com>
2019-08-31 22:03:36 +0800

    ssl support

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 Jetty Reverse Proxy example
 
 
-## Generate keystore
+## Generate server keystore jks
 
 keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
+
+## Generate personal cert
+keytool -genkey -alias my-name \
+    -keystore cert.pfx \
+    -storetype pkcs12 \
+    -keyalg RSA \
+    -storepass secret123 \
+    -validity 730 \
+    -keysize 2048 
--- src/main/java/com/example/ReverseProxy.java
+++ src/main/java/com/example/ReverseProxy.java
@@ -1,10 +1,73 @@
 package com.example;
 
+import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.proxy.ConnectHandler;
+import org.eclipse.jetty.proxy.ProxyServlet;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+
+import javax.security.auth.x500.X500Principal;
+import javax.servlet.http.HttpServletRequest;
+import java.security.cert.X509Certificate;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  */
 public class ReverseProxy {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server();
+
+        // HTTPS configuration
+        HttpConfiguration https = new HttpConfiguration();
+        https.addCustomizer(new SecureRequestCustomizer());
+
+        // Configuring SSL
+        SslContextFactory sslContextFactory = new SslContextFactory();
+
+        // Defining keystore path and passwords
+        sslContextFactory.setKeyStorePath(ReverseProxy.class.getResource("/my-key.jks").toExternalForm());
+        sslContextFactory.setKeyStorePassword("reverse123");
+        sslContextFactory.setKeyManagerPassword("reverse123");
+        sslContextFactory.setNeedClientAuth(true);
+
+        // Configuring the connector
+        ServerConnector sslConnector = new ServerConnector(server, new SslConnectionFactory(sslContextFactory, "http/1.1"), new HttpConnectionFactory(https));
+        sslConnector.setPort(8443);
+
+        server.setConnectors(new Connector[]{sslConnector});
+
+        ConnectHandler proxy = new ConnectHandler();
+        server.setHandler(proxy);
+        ServletContextHandler context = new ServletContextHandler(proxy, "/", ServletContextHandler.SESSIONS);
+        ServletHolder proxyServlet = new ServletHolder(PCFReverseProxy.class);
+        proxyServlet.setInitParameter("proxyTo", "http://localhost:8080/");
+        proxyServlet.setInitParameter("prefix", "/");
+        context.addServlet(proxyServlet, "/*");
+
+        server.start();
+    }
+
+    public static class PCFReverseProxy extends ProxyServlet.Transparent {
+        private final Logger log = Logger.getLogger(PCFReverseProxy.class.getName());
+
+        @Override
+        protected void addProxyHeaders(HttpServletRequest clientRequest, Request proxyRequest) {
+            super.addProxyHeaders(clientRequest, proxyRequest);
+
+            X509Certificate certs[] =
+                    (X509Certificate[])clientRequest.getAttribute("javax.servlet.request.X509Certificate");
+            if (certs != null && certs.length > 0) {
+                X509Certificate clientCert = certs[0];
+                X500Principal subjectDN = clientCert.getSubjectX500Principal();
+                log.info("Subject " + subjectDN.getName());
+            } else {
+                log.info("No cert");
+            }
+        }
     }
 }
