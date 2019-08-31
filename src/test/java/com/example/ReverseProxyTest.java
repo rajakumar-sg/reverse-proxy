34b8f329d2e375da5932176c5e2da8dc2c045e24
raj <torajakumar@gmail.com>
2019-08-31 12:17:24 +0800

    init code base

---
+++ src/test/java/com/example/ReverseProxyTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple ReverseProxy.
+ */
+public class ReverseProxyTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
